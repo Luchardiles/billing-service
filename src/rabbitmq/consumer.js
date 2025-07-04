1e691f4d994d281f55ea9397814863eb16e7fa38
Luchardiles <luiis_rapero@hotmail.cl>
2025-07-04 16:49:58 -0400

    refactor: Update user event queues and add error handling

--- src/rabbitmq/consumer.js
+++ src/rabbitmq/consumer.js
@@ -1,20 +1,30 @@
 const prisma = require ("../database/prisma")
 const {connectRabbit} = require("./connection");
-const {userCreatedQueue, userDeletedQueue, emailUpdatedQueue} = require("../config/env");
+const {userCreatedQueueBilling, userDeletedQueueBilling} = require("../config/env");
 
 async function consumeUserEvents() {
     const ch = await connectRabbit();
-    await ch.assertQueue(userCreatedQueue, { durable: true });
-    await ch.assertQueue(userDeletedQueue, { durable: true });
-    ch.consume(userCreatedQueue, async (msg) => {
-        const user = JSON.parse(msg.content.toString());
-        await prisma.user.create({ data: user });
-        ch.ack(msg);
+    await ch.assertQueue(userCreatedQueueBilling, { durable: true });
+    await ch.assertQueue(userDeletedQueueBilling, { durable: true });
+    ch.consume(userCreatedQueueBilling, async (msg) => {
+        try {
+            const user = JSON.parse(msg.content.toString());
+            await prisma.user.create({ data: user });
+            ch.ack(msg);
+        } catch (err) {
+            console.error("Error processing user created event:", err);
+            ch.nack(msg, false, false);
+        }
     });
-    ch.consume(userDeletedQueue, async (msg) => {
-        const userId = msg.content.toString();
-        await prisma.user.delete({ where: { id: userId } });
-        ch.ack(msg);
+    ch.consume(userDeletedQueueBilling, async (msg) => {
+        try {
+            const user = JSON.parse(msg.content.toString());
+            await prisma.user.delete({ where: { id: user.id } });
+            ch.ack(msg);
+        } catch (err) {
+            console.error("Error processing user deleted event:", err);
+            ch.nack(msg, false, false);
+        }
     });
 
 }
