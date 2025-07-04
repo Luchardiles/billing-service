d84d30d5586c4ea710f228bc6d7570e97a445415
Luchardiles <luiis_rapero@hotmail.cl>
2025-07-04 17:08:17 -0400

    fix: changed environment variable names and database port

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,9 @@ module.exports = {
   port: process.env.PORT || 50051,
   serverUrl: process.env.SERVER_URL || "0.0.0.0",
   rabbitUrl: process.env.RABBITMQ_URL || "amqp://localhost",
-  userCreatedQueue: process.env.USER_CREATED_QUEUE || "user.created",
-  userDeletedQueue: process.env.USER_DELETED_QUEUE || "user.deleted",
+  userCreatedQueueBilling:
+    process.env.USER_CREATED_QUEUE_BILLING || "user.created.billing",
+  userDeletedQueueBilling:
+    process.env.USER_DELETED_QUEUE_BILLING || "user.deleted.billing",
   emailUpdatedQueue: process.env.EMAIL_UPDATED_QUEUE || "email.updated",
 };
